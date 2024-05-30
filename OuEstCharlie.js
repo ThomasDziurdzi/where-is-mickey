1eccdba85a9de726be074a5772e729e50897a996
MacBook Pro Sandrine CP <macbookprosandrinecp@MacBook-Pro-de-MacBook.local>
2024-05-30 16:37:33 +0200

    add dialog box in html, css and js

---
+++ OuEstCharlie.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionne les éléments du DOM
+    const herculeButton = document.getElementById('herculeButton');
+    const dialogBoxHercule = document.getElementById('dialogBoxHercule');
+
+    // Événement de survol pour afficher la boîte de dialogue
+    herculeButton.addEventListener('mouseover', function() {
+        dialogBoxHercule.classList.remove('hidden');
+    });
+
+    // Événement de sortie pour cacher la boîte de dialogue
+    herculeButton.addEventListener('mouseout', function() {
+        dialogBoxHercule.classList.add('hidden');
+    });
+});
