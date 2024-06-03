59a19a827314651b37967e2121a22a77ee18f07f
MacBook Pro Sandrine CP <macbookprosandrinecp@MacBook-Pro-de-MacBook.local>
2024-06-03 17:32:51 +0200

    changes on js file trying to debug dialog box

--- script/ouEstCharlie.js
+++ script/ouEstCharlie.js
@@ -1,16 +1,25 @@
-//attendre que la page soit chargée
+// //attendre que la page soit chargée
 document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionne les éléments du DOM
-    const herculeButton = document.getElementById('herculeButton');
-    const dialogBoxHercule = document.getElementById('dialogBoxHercule');
+//     // trouver le bouton et la boîte de dialogue dans le DOM
+//const herculeButton = document.getElementById('herculeFoireux');
+//     //herculeFoireux = id de l'image dans le html
+//const dialogBoxHercule = document.getElementById('dialogBoxHercule');
 
-    // Événement de survol pour afficher la boîte de dialogue
-    herculeButton.addEventListener('mouseover', function() {
-        dialogBoxHercule.classList.remove('hidden');
-    });
+//     // montrer la boîte de dialogue quand on passe la souris sur le bouton
+//     herculeButton.addEventListener('click', function() {
+//         console.log('Image clicked'); //log pour vérifier que l'événement est déclenché
+//         dialogBoxHercule.classList.remove('hidden');//enlever la classe 'hidden' de la boîte de dialogue pour qu'elle apparaisse
+//     });
 
-    // Événement de sortie pour cacher la boîte de dialogue
-    herculeButton.addEventListener('mouseout', function() {
-        dialogBoxHercule.classList.add('hidden');
-    });
-});
+//     // cacher la boîte de dialogue quand on enlève la souris
+//     herculeButton.addEventListener('click', function() {
+//         console.log('Dialog box clicked');//log pour vérifier que l'événement est déclenché
+//         dialogBoxHercule.classList.add('hidden');//ajoute la classe 'hidden' de la boîte de dialogue pour qu'elle disparaisse
+//     });
+// });
+const herculeImage = document.getElementById('herculeFoireux');
+const dialogBox = document.getElementById('dialogBoxHercule');
+
+herculeImage.addEventListener('click', function(){
+    dialogBox.classList.remove('hidden');
+})
