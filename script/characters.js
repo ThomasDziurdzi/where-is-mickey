d0816f46d9ec3fa4a5460ba0ed9020d0425a6cf7
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-04 16:57:11 +0200

    adding modal div

---
+++ script/characters.js
@@ -0,0 +1,13 @@
+characters = [{
+    id: 1,
+    name: 'Hercule',
+    description: `I am the Doctor, and you are the Daleks! You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better? Sorry, checking all the water in this area; there's an escaped fish.`,
+    img: "./images/hercule.png",
+},
+{
+    id: 2,
+    name: 'Minie',
+    description: `Saving the world with meals on wheels. You've swallowed a planet! I hate yogurt. It's just stuff with bits in. You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better?`,
+    img: './images/menu/minnie\ diva.png'
+}
+]
--- script/index.js
+++ script/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const columns = document.querySelectorAll('.columns');
     const columnsContainer = document.querySelector('.columns-container');
+    const begin = document.getElementById('begin');
+    console.log(begin)
+   
 
     columns.forEach(column => {
         //Itère sur chaque élément de la nodelist et ajoute un eventlistener
@@ -8,13 +11,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             columns.forEach(col => {
                 //vérifie la condition à chaque survol d'une colonne
                 if (col !== column) {
-                    col.style.filter = 'grayscale(100%) opacity(20%)';
+                    col.style.filter = 'grayscale(100%) opacity(20%) blur(2px)';
                     col.classList.add('transition');
                 }
             })
         })
 
-        column.addEventListener('mouseout', function() {
+        column.addEventListener('mouseout', function () {
             columns.forEach(col => {
                 col.style.filter = '';
             })
@@ -22,10 +25,55 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     //On s'assure qu'une fois quitté le conteneur, plus aucun filtre n'est actif
-    columnsContainer.addEventListener('mouseleave', function() {
+    columnsContainer.addEventListener('mouseleave', function () {
         columns.forEach(column => {
             column.style.filter = '';
         })
     })
+
+//Crée un fondu noir pour passer d'une page à l'autre
+    function buttonTransition(classButton, overlay) {
+        const transitionLink = document.querySelector(`.${classButton}`);
+        const overlayAnimation = document.querySelector(`.${overlay}`);
+
+        if (transitionLink) {
+            transitionLink.addEventListener('click', (e) => {
+                e.preventDefault();
+                const targetHref = transitionLink.getAttribute('href');
+
+                overlayAnimation.classList.add('active');
+
+                setTimeout(() => {
+                    window.location.href = targetHref;
+                }, 1000); // Correspond à la durée de l'animation CSS
+            });
+        }
+    }
+
+    buttonTransition('transition-button', 'overlayPage');
+
+
+//-------------------------MODALE--------------------------
+const modal = document.querySelector('.modal');
+const overlayColumn = document.querySelector('.overlayColumn');
+const firstImg = document.querySelector('.sixth');
+const closeModalBtn = document.querySelector('.btn-close');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlayColumn.classList.remove('hidden');
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlayColumn.classList.add('hidden');
+}
+
+
+
+firstImg.addEventListener('click', openModal)
+closeModalBtn.addEventListener('click',closeModal)
+
+
 });
 
