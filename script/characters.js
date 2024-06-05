7a09f8c52b4cf6eae515dba2c9502293513beecb
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-06 00:44:46 +0200

    minor changes

--- script/characters.js
+++ script/characters.js
@@ -3,42 +3,42 @@ const characters = [
         id: 1,
         name: 'Hercule',
         content: `I am the Doctor, and you are the Daleks! You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better? Sorry, checking all the water in this area; there's an escaped fish.`,
-        img: "./images/hercule.png",
+        image: "./images/hercule.png",
         imgCut: "./images/menu/herculecutcolumn.png"
     },
     {
         id: 2,
         name: 'Minie',
         description: `Saving the world with meals on wheels. You've swallowed a planet! I hate yogurt. It's just stuff with bits in. You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better?`,
-        img: './images/menu/minnie\ diva.png',
+        image: './images/menu/minnie.png',
         imgCut: './images/menu/minniecutcolumns.png'
     },
     {
         id : 3,
         name : 'Bugs Bunny',
         content : 'dqsdqsdqsdqdqsdqsd',
-        img: './images/menu/bugs.jpeg',
+        image: './images/menu/bugs.jpeg',
         imgCut : 'images/menu/bugscolumns.png'
     },
     {
         id: 4,
         name: 'Daffy Duck',
         content: "qsdqdqfggsdf",
-        img: './images/menu/daffy-duck-wearing-as-a-bartender-he-looks-completely-stupid-both-of-his-eyes-is-looking-in-a-diff.jpeg',
+        image: './images/menu/daffy-duck-wearing-as-a-bartender-he-looks-completely-stupid-both-of-his-eyes-is-looking-in-a-diff.jpeg',
         imgCut: 'images/menu/daffycutcolumns.png'
     },
     {
         id: 5,
         name: 'Franklin',
         content: `I'm a crackhead`,
-        img: './images/menu/franklinsdf.png',
+        image: './images/menu/franklinsdf.png',
         imgCut : './images/menu/franklinsdfcutcolumns.png'
     },
     {
         id: 6,
         name: 'Pork',
         content: 'sddaaaaazeazefsdfs',
-        img: './images/menu/porkeatingbacon.jpeg',
+        image: './images/menu/porkeatingbacon.jpeg',
         imgCut: './images/menu/porkcutcolumns.png'
     }
 ]
--- script/index.js
+++ script/index.js
@@ -2,38 +2,54 @@ import characters from './characters.js';
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
-     // creating columns of characters
-     const columnContainer = document.querySelector('.columns-container');
-     
-     function createColumn(image) {
-         const columnCharacter = document.createElement('div');
-         columnCharacter.classList.add('column')
-         columnCharacter.style.backgroundImage = `url(${image})`
-         columnContainer.appendChild(columnCharacter);
-     }
+    // creating columns of characters
+    const columnContainer = document.querySelector('.columns-container');
 
-     for(let i = 0; i < characters.length; i++) {
-         createColumn(characters[i].imgCut)
-     }
+    function createColumn(image) {
+        const columnCharacter = document.createElement('div');
+        columnCharacter.classList.add('column');
+        columnCharacter.style.backgroundImage = `url(${image})`;
+        columnContainer.appendChild(columnCharacter);
+    }
+    for (let i = 0; i < characters.length; i++) {
+        createColumn(characters[i].imgCut)
+        
+    }
 
-     //filters when mouseover and mouseout on columns
-     //Opening Modal
-     const column = document.querySelectorAll('.column');
-     const modal = document.querySelector('.modal'); 
-     const overlayPage = document.querySelector('.overlayPage');
+    const beforeDiv = document.querySelector('.overlayModal')
+    function createModal(image){
+        const modal = document.createElement('div');
+        modal.classList.add('modal')
+        modal.classList.add('hidden')
+        modal.style.backgroundImage = `url(${image})`;
+        beforeDiv.insertAdjacentElement("afterend", modal);
+    }
+    for (let i = 0; i < characters.length; i++) {
+        createModal(characters[i].image)
+    }
+    //filters when mouseover and mouseout on columns
+    //Opening Modal
+    const column = document.querySelectorAll('.column');
+    const modal = document.querySelector('.modal');
+    const overlayModal = document.querySelector('.overlayModal');
 
-     column.forEach((col) => {
+    column.forEach((col) => {
         //FILTERS
-        col.addEventListener('mouseover' , function() {
+        col.addEventListener('mouseover', function () {
             col.style.filter = 'grayscale(100%) opacity(0.6) blur(2px)';
         })
         //MODAL
-        col.addEventListener('click', function() {
+        col.addEventListener('click', function () {
+            // Get the index from the data attribute
+            const index = col.dataset.index;
+           // Find the corresponding character object
+           const character = characters[index];
             modal.classList.remove('modal-hidden')
-            overlayPage.
-            
-        })
-     })
+            overlayModal.classList.remove('modal-hidden')
+            modal.style.backgroundImage = `url(${character.imgCut})`;
+           
+        });
+    });
 
 
     // function attachEventListeners(column) {
@@ -118,8 +134,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //     column.addEventListener('click', openModal);
     //     closeModalBtn.addEventListener('click', closeModal);
     // }
-       
 
-       
 
-    });
+
+
+});
