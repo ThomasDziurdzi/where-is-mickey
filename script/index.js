640e4a781c91f90ba70c6d372c3c8aeeab6190c1
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-06 13:36:18 +0200

    starting commencer button animation

--- script/index.js
+++ script/index.js
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     col.style.transform = '';
                 } else {
                     col.style.filter = ''
+                    column.style.transform = 'scale(1.05)';
                 }
             })
             columnContainer.addEventListener('mouseleave', function () {
@@ -37,7 +38,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
         })
     });
-
 
     //Modal opening and closing
     const modal = document.querySelector('.modal');
@@ -80,50 +80,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     })
 
 
-
-
-
-    // function attachEventListeners(column) {
-    //     column.addEventListener('mouseover', function () {
-    //         let modifiedColumns = [];
-    //         columnsContainer.querySelectorAll('.columns').forEach(col => {
-    //             if (col !== column) {
-    //                 col.style.filter = 'grayscale(100%) opacity(0.6) blur(2px)';
-                    col.classList.add('transition');
-                    col.style.transform = '';
-    //                 modifiedColumns.push(col);
-    //             }
-    //         });
-
-    //         column.style.transform = 'scale(1.05)';
-
-    //         column.addEventListener('mouseout', function () {
-    //             modifiedColumns.forEach(col => {
-    //                 col.style.filter = '';
-    //                 col.style.transform = '';
-    //             });
-    //             column.style.transform = '';
-    //         }, { once: true });
-    //     });  
-    // }
-
-    columnsContainer.addEventListener('mouseleave', function () {
-        columnsContainer.querySelectorAll('.columns').forEach(column => {
-            column.style.filter = '';
-            column.style.transform = '';
-        });
-    });
-
-    // characters.forEach((character) => {
-    //     const columnDiv = document.createElement('div');
-    //     columnDiv.classList.add('columns');
-    //     columnDiv.style.backgroundImage = `url(${character.imgCut})`;
-    //     columnsContainer.appendChild(columnDiv);
-
-    //     attachEventListeners(columnDiv);
-    //     attachEventListenersModal(columnDiv, character.img); // Attach modal event listener here
-    // });
-
+    //Transition for main page to ruelle page
     function buttonTransition(classButton, overlay) {
         const transitionLink = document.querySelector(`.${classButton}`);
         const overlayAnimation = document.querySelector(`.${overlay}`);
@@ -143,30 +100,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     buttonTransition('transition-button', 'overlayPage');
-
-    // function attachEventListenersModal(column, imgSrc) {
-    //     const modal = document.querySelector('.modal');
-    //     const overlayColumn = document.querySelector('.overlayColumn');
-    //     const closeModalBtn = document.querySelector('.btn-close');
-    //     // const img = document.querySelector('.fullCharacter');
-
-    //     const openModal = function () {
-    //         const characterImg = document.createElement("img");
-    //         characterImg.src = imgSrc; // Set the image source for the modal
-    //         modal.classList.remove('hidden');
-    //         overlayColumn.classList.remove('hidden');
-    //     };
-
-    //     const closeModal = function () {
-    //         modal.classList.add('hidden');
-    //         overlayColumn.classList.add('hidden');
-    //     };
-
-    //     column.addEventListener('click', openModal);
-    //     closeModalBtn.addEventListener('click', closeModal);
-    // }
-
-
-
-
 });
+
+
+    
+
+
+
+
+
