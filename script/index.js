b8e0534832e6705febae51b07c22be0b86a3ca27
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-06 12:33:27 +0200

    finished character modal + starting animation on columns

--- script/index.js
+++ script/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const columnCharacter = document.createElement('div');
         columnCharacter.classList.add('columns');
         columnCharacter.style.backgroundImage = `url(${image})`;
-        columnCharacter.dataset.index = index; 
+        columnCharacter.dataset.index = index;
         columnContainer.appendChild(columnCharacter);
     }
     for (let i = 0; i < characters.length; i++) {
@@ -18,36 +18,70 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     //filters when mouseover and mouseout on columns
     const columns = document.querySelectorAll('.columns');
-    //Opening or closing modal
+    columns.forEach((column) => {
+        column.addEventListener('mouseover', function () {
+            columns.forEach((col) => {
+                if (col !== column) {
+                    col.style.filter = 'grayscale(100%) opacity(0.6) blur(2px)';
+                    col.classList.add('transition');
+                    col.style.transform = '';
+                } else {
+                    col.style.filter = ''
+                }
+            })
+            columnContainer.addEventListener('mouseleave', function () {
+                columnContainer.querySelectorAll('.columns').forEach(column => {
+                    column.style.filter = '';
+                    column.style.transform = '';
+                });
+            });
+        })
+    });
+
+
+    //Modal opening and closing
     const modal = document.querySelector('.modal');
-    //Opening or closing overlay modal
     const closeModal = document.querySelector('.modal');
     const overlayModal = document.querySelector('.overlayModal');
+    columns.forEach((column) => {
 
-
-    columns.forEach((col) => {
-        //FILTERS
-        col.addEventListener('mouseover', function () {
-            col.style.filter = 'grayscale(100%) opacity(0.6) blur(2px)';
-        })
-        //MODAL
-        col.addEventListener('click', function () {
+        //MODAL OPENING
+        column.addEventListener('click', function (event) {
+            //prevent for automatically closing modal
+            event.stopPropagation();
             // Get the index from the data attribute
-            const index = col.dataset.index;
-           // Find the corresponding character object
-           const character = characters[index];
+            const index = column.dataset.index;
+            // Find the corresponding character object
+            const character = characters[index];
             modal.classList.remove('hidden')
             // modalImage.classList.remove('hidden')
             overlayModal.classList.remove('hidden')
             modal.style.backgroundImage = `url(${character.image})`
         });
-    });
 
-   modal.removeEventListener('keydown', function(e){
-        if (e.key === 'Escape') {
-            modal.classList.add('hidden')
-        }
-   }) 
+        //MODAL CLOSING
+        document.addEventListener('click', function () {
+            if (!modal.classList.contains('hidden')) {
+                //adding class fade.out for playing animation
+                modal.classList.add('fade-out');
+                overlayModal.classList.add('fade-out');
+
+                setTimeout(() => {
+                    //when animation has ending, hiding modal and removing
+                    //fade-out class
+                    modal.classList.add('hidden');
+                    overlayModal.classList.add('hidden');
+                    modal.style.backgroundImage = '';
+                    modal.classList.remove('fade-out');
+                    overlayModal.classList.remove('fade-out');
+                }, 300);
+            }
+        })
+    })
+
+
+
+
 
     // function attachEventListeners(column) {
     //     column.addEventListener('mouseover', function () {
@@ -55,8 +89,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //         columnsContainer.querySelectorAll('.columns').forEach(col => {
     //             if (col !== column) {
     //                 col.style.filter = 'grayscale(100%) opacity(0.6) blur(2px)';
-    //                 col.classList.add('transition');
-    //                 col.style.transform = '';
+                    col.classList.add('transition');
+                    col.style.transform = '';
     //                 modifiedColumns.push(col);
     //             }
     //         });
@@ -73,12 +107,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //     });  
     // }
 
-    // columnsContainer.addEventListener('mouseleave', function () {
-    //     columnsContainer.querySelectorAll('.columns').forEach(column => {
-    //         column.style.filter = '';
-    //         column.style.transform = '';
-    //     });
-    // });
+    columnsContainer.addEventListener('mouseleave', function () {
+        columnsContainer.querySelectorAll('.columns').forEach(column => {
+            column.style.filter = '';
+            column.style.transform = '';
+        });
+    });
 
     // characters.forEach((character) => {
     //     const columnDiv = document.createElement('div');
